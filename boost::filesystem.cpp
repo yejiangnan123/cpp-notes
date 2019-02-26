c378aaa3af2c6155413dd375fccfd3f32da27df9
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-02-26 10:03:27 +0800

    Update boost::filesystem.cpp

--- boost::filesystem.cpp
+++ boost::filesystem.cpp
@@ -1,35 +1,39 @@
 文件系统常用函数
 boost::filesystem;
-std::fstream
-
-
-namespace fc=boost::filesystem;//简单别名
-#define LOG_READ  (std::ios::in | std::ios::binary)
-#define LOG_WRITE (std::ios::out | std::ios::binary | std::ios::app)
-std::fstream             block_stream;
-if (my->block_stream.is_open())
-         my->block_stream.close();//判断文件是否打开
-if (!fc::is_directory(data_dir))
-         fc::create_directories(data_dir);判断目录是否存在
-my->block_file = data_dir / "blocks.log";//文件名拼接
 my->block_stream.open(my->block_file.generic_string().c_str(), LOG_WRITE);  //写模式打开文件
-auto log_size = fc::file_size(my->block_file);   //计算文件大小
-block_stream.close();关闭文件
-block_stream.open(block_file.generic_string().c_str(), LOG_WRITE);打开文件
-my->block_stream.exceptions(std::fstream::failbit | std::fstream::badbit);
-my->block_stream.seekg( 0 );定位文件光标
-my->block_stream.read( (char*)&my->version, sizeof(my->version) ); 读数据到变量
-if (my->block_stream.tellg() <= sizeof(pos))   当前位置
-my->block_stream.seekg(-sizeof(pos), std::ios::end);设置位置
-fc::remove_all(my->index_file);移除文件
-my->block_stream.write((char*)&pos, sizeof(pos));写数据
-my->block_stream.flush();同步缓存
+
 std::is_same 判断类型一致性
 std::decay 退化修饰
-fc::raw::unpack(old_block_stream, gs);
-auto data = fc::raw::pack( gs );
-std::exception_ptr     except_ptr;
-uint64_t tmp_pos = std::numeric_limits<uint64_t>::max();
-pos = new_block_stream.tellp();  光标位置
-auto tail_path = blocks_dir / std::string("blocks-bad-tail-").append( now ).append(".log");
-         if( !fc::exists(tail_path) && incomplete_block_data.size() > 0 )
+
+const std::string& string( )： 字符串化
+std::string root_directory( )：  根目录
+std::string root_name( )：根目录名
+std::string leaf( )：绝对路径
+std::string branch_path( )：路径名
+bool empty( )：空
+boost::filesystem::path::iterator：路径迭代器
+boost::filesystem::rename(path, path2);  重命名
+catch(boost::filesystem::filesystem_error e) {   错误，异常 
+uintmax_t file_size(const path&)：文件大小
+boost::filesystem::space_info space(const path&)：磁盘空间
+std::time_t last_write_time(const path&)：最后修改时间。
+void last_write_time(const path&, std::time_t new_time)：最后修改时间。
+const path& current_path( )：当前工作目录的完整路径
+bool create_directory(const path&)：创建目录
+bool create_directories(const path&)：创建目录树
+bool create_hard_link (const path& frompath, const path& topath)：创建硬链接。
+bool create_symlink(const path& frompath, const path& topath)：创建符号（软）链接。
+void copy_file(const path& frompath, const path& topath)：复制
+void rename(const path& frompath, const path& topath)：重命名
+bool remove(const path& p)：删除
+unsigned long remove_all(const path& p)：删除
+bool exists(const path&)：检查文件的扩展名
+bool is_directory(const path&)：检查目录
+bool is_regular(const path&)：检查普通文件
+bool is_other(const path&)：检查设备文件
+bool is_empty(const path&)：检查是否为空
+bool equivalent(const path1& p1, const path2& p2)：比较路径
+path system_complete(const path&)：绝对路径
+std::string extension(const path&)：扩展名
+std::string basename(const path&)：文件名，不带扩展名
+std::string change_extension(const path& oldpath, const std::string new_extension)：修改扩展名
