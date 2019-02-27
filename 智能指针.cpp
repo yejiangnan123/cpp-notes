8d760e42de49c89e1d2c98539b02ae79b099b22b
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-02-27 10:12:04 +0800

    Create 智能指针.cpp

---
+++ 智能指针.cpp
@@ -0,0 +1,13 @@
+智能指针
+shared_ptr、scoped_ptr、intrusive_ptr、weak_ptr、shared_array、scoped_array
+
+https://blog.csdn.net/dongguan131/article/details/6683843 
+------------------------
+boost作用域指针    https://blog.csdn.net/peterpan_hai/article/details/51704588    独占
+boost::scoped_array   https://blog.csdn.net/ajioy/article/details/7353477   独占
+boost共享指针    https://www.cnblogs.com/helloamigo/p/3575098.html  共享
+boost共享数组    https://blog.csdn.net/peterpan_hai/article/details/51726690 共享
+boost弱指针     https://blog.csdn.net/acs713/article/details/29175231  和共享指针一起使用，不改变引用计数
+boost侵入式智能指针intrusive_ptr   https://blog.csdn.net/caimouse/article/details/8631381  自定义智能指针类   
+boost指针容器    http://blog.chinaunix.net/uid-29073321-id-5585171.html   独占
+------------------------
