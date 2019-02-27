b4e3008861451caa3132f70a627ff5083361b67f
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-02-27 16:13:25 +0800

    Update log.cpp

--- log.cpp
+++ log.cpp
@@ -1,6 +1,7 @@
 log 日志库
 ----------------------
 boost log日志看
+https://www.boost.org/doc/libs/1_69_0/libs/log/doc/html/log/tutorial.html#log.tutorial.trivial
 ----------------------
 很多第三方日志库
 ----------------------
