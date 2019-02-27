3c910a8ea6fba886cb631a37e5110cc8c5dc672b
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-02-27 15:04:31 +0800

    Create boost-http.cpp

---
+++ boost-http.cpp
@@ -0,0 +1,15 @@
+boost 1.66版本后增加http库
+https://www.boost.org/doc/libs/1_69_0/libs/beast/doc/html/index.html
+------------------------------------
+http 客户端 同步
+https://www.boost.org/doc/libs/1_69_0/libs/beast/doc/html/beast/quick_start.html#beast.quick_start.http_client
+------------------------------------
+http 客户端 异步
+https://www.boost.org/doc/libs/1_69_0/libs/beast/example/http/client/async/http_client_async.cpp
+------------------------------------
+http 服务器 同步
+https://www.boost.org/doc/libs/1_69_0/libs/beast/example/http/server/sync/http_server_sync.cpp
+------------------------------------
+http 服务器 异步
+https://www.boost.org/doc/libs/1_69_0/libs/beast/example/http/server/async/http_server_async.cpp
+------------------------------------
