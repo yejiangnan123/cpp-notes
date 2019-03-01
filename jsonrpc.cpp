f15c87127cd456c26804e5cbee2aebc28d322ee0
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-03-01 11:31:00 +0800

    Update jsonrpc.cpp

--- jsonrpc.cpp
+++ jsonrpc.cpp
@@ -3,3 +3,5 @@ jsonrpc 2.0
 ----------------------------
 https://github.com/uskr/jsonrpc-lean
 ----------------------------
+  https://github.com/badaix/jsonrpcpp
+--------------------------------------
