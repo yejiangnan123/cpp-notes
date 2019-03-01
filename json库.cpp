fc10aaa7d99ebb99a313fd1f05c639d4f2f8a2f0
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-03-01 16:43:52 +0800

    Update json库.cpp

--- json库.cpp
+++ json库.cpp
@@ -9,4 +9,8 @@ https://www.cnblogs.com/liaocheng/p/4243731.html
 反序列化json ：
 删除json子对象 ：removeMember
 利用jsoncpp将json字符串转换为Vector ：
+https://www.cnblogs.com/bhlsheji/p/4818221.html
+https://www.cnblogs.com/wanghuixi/p/7685444.html
+sudo apt-get install libjsoncpp-dev libjsoncpp0 
+#include <jsoncpp/json/json.h> 
 --------------------------
