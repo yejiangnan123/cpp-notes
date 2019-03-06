9896464228da361c0f4aeeb855b0b1909cffee7b
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-03-06 14:56:29 +0800

    Create std::algorithm.cpp

---
+++ std::algorithm.cpp
@@ -0,0 +1,102 @@
+-----------------------------------------------------
+std::algorithm 算法函数
+http://www.cplusplus.com/reference/algorithm/
+-----------------------------------------------------
+Non-modifying sequence operations: 不修改序列的操作
+all_of 
+any_of 
+none_of 
+for_each
+find
+find_if
+find_if_not 
+find_end
+find_first_of
+adjacent_find
+count
+count_if
+mismatch
+equal
+is_permutation 
+search
+search_n
+----------------------------------------------------
+Modifying sequence operations:修改序列的操作
+Copy
+copy_n 
+copy_if 
+copy_backward
+move 
+move_backward 
+swap
+swap_ranges
+iter_swap
+transform
+replace
+replace_if
+replace_copy
+replace_copy_if
+fill
+fill_n
+generate
+generate_n
+remove
+remove_if
+remove_copy
+remove_copy_if
+unique
+unique_copy
+reverse
+reverse_copy
+rotate
+rotate_copy
+random_shuffle
+shuffle 
+--------------------------------------------
+Partitions:   分区
+is_partitioned 
+partition
+stable_partition
+partition_copy 
+partition_point 
+--------------------------------------------
+Sorting:       排序
+sort
+stable_sort
+partial_sort
+partial_sort_copy
+is_sorted 
+is_sorted_until 
+nth_element
+--------------------------------------------
+Binary search：  二进制搜索
+lower_bound
+upper_bound
+equal_range
+binary_search
+--------------------------------------------
+Merge   合并
+merge
+inplace_merge
+includes
+set_union
+set_intersection
+set_difference
+set_symmetric_difference
+-----------------------------------------------
+Heap:  堆
+push_heap
+pop_heap
+make_heap
+sort_heap
+is_heap 
+is_heap_until 
+-----------------------------------------------
+Min/max:   最小最大值
+min
+max
+minmax 
+min_element
+max_element
+minmax_element 
+-----------------------------------------------
