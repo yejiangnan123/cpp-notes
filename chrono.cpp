c07de3c9c31afc776ae8fb67a41dc4548b368196
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-03-08 11:13:27 +0800

    Create chrono.cpp

---
+++ chrono.cpp
@@ -0,0 +1,19 @@
+-------------------------------------------
+std::chrono   计时
+http://www.cplusplus.com/reference/chrono/
+-------------------------------------------
+duration  这是一个类，表示时间段，单位可以是秒，微妙，毫秒，纳秒
+zero      0值函数，可以用于比较
+min       最小值
+max       最大值
+time_point  这是一个类，表示时间
+now      当前时间
+time_since_epoch  把时间转换成时间段
+system_clock   时钟类
+now
+to_time_t
+from_time_t
+steady_clock   这是一个类，用来计算时间间隔
+duration_cast   持续时间计算
+time_point_cast
+-----------------------------------------------
