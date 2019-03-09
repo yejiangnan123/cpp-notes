b33069f2736a61dcc53d6bacc39afcdd3f286512
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-03-09 10:35:50 +0800

    Create std::condition_variable.cpp

---
+++ std::condition_variable.cpp
@@ -0,0 +1,7 @@
+------------------------------------------
+std::condition_variable 条件变量
+http://www.cplusplus.com/reference/condition_variable/
+------------------------------------------
+condition_variable  这是一个类，管理条件变量，一个线程等待条件，一个线程设置条件并通知等待的线程；只能绑定unique_lock<mutex>
+condition_variable_any  这也是一个条件变量，但是可以绑定人意类型的锁
+------------------------------------------
