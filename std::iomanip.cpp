3a4593021174f2c59967d771f89e9e69827ea14f
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-03-09 11:15:06 +0800

    Create std::iomanip.cpp

---
+++ std::iomanip.cpp
@@ -0,0 +1,15 @@
+------------------------------------------------------
+std::iomanip  设置输出格式
+http://www.cplusplus.com/reference/iomanip/
+------------------------------------------------------
+setiosflags  设置
+resetiosflags  重新设置
+setbase     进制
+setfill     填充
+setprecision   小数精度
+setw         宽度
+get_money     输入货币
+put_money     输出货币
+get_time      输入时间
+put_time     输出时间
+-----------------------------------------------------------
