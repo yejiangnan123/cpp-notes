2fe8f4020f90b6426f4a765d019d530e024709a1
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-03-11 17:41:12 +0800

    Create std::utility.cpp

---
+++ std::utility.cpp
@@ -0,0 +1,10 @@
+----------------------------------------------------
+std::utility
+http://www.cplusplus.com/reference/utility/
+----------------------------------------------------
+swap        交换
+make_pair   键值对
+forward     完美转发，数据内容和数据类型，右值引用
+move        移动，效率高，但是原来的资源会消失
+move_if_noexcept 
+declval 
